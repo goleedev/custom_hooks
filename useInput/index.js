03c32bf495d6ddb51db763985ee1e97fccf56197
GO Lee <golee.dev@gmail.com>
2021-01-20 21:08:27 +0900

    0.1 useInput

---
+++ useInput/index.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import useInput from "./useInput";
+
+function App() {
+  const maxLen = (value) => value.length <= 10;
+  const name = useInput("Ms. ", maxLen);
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <input placeholder="Name" {...name} />
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
