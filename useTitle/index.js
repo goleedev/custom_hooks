9ac0cb7404dadba1b25fa075355c55ec43da81cd
GO Lee <golee.dev@gmail.com>
2021-01-23 22:24:22 +0900

    0.4 useTitle

---
+++ useTitle/index.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import useTitle from "./useTitle";
+
+function App() {
+  const titleUpdater = useTitle("Loading...");
+  setTimeout(() => titleUpdater("Home"), 5000);
+  return <div className="App"></div>;
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
